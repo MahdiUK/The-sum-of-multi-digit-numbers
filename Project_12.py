ab83d55fbd977819bb1503f9c13592a3acb30369
Mahdi <123685546+MahdiUK@users.noreply.github.com>
2023-01-29 20:32:40 +0000

    Update Project_12.py

--- Project_12.py
+++ Project_12.py
@@ -1,9 +1,16 @@
+# Solution 1
 a = int(input('Enter Your Number: '))
 b = 0
 while a:
     a , remainder = divmod(a , 10)
     b += remainder
+
+print('Sum of your number:' , b)
+
+# Solution 2
+number = int(input('Enter Your Number: '))
+SUM = 0
+for items in str(number):
+    SUM += int(items)
     
-
-print(f'Sum of your number: {b}')
-
+print('Sum of your number:', SUM)
